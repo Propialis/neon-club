ac1cac98e31e16c70289436784d0a340985a9325
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-14 21:59:12 -0400

    minor alteration agian

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ export default (e) => {
         uTime: { value: 0 },
         uPulse: { value: 2 },
         uPulse2: { value: -2 },
-        uBeat: { value: -4 },
+        uBeat: { value: 2 },
         uMood: { value: new THREE.Vector3(0.1, 0.2, 0.6) },
         uResolution: {
           value: new THREE.Vector2(window.innerWidth, window.innerHeight),
