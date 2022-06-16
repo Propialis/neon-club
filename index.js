ac75344c962956bb0dd19b76e93c22273e5ed8e4
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-16 03:31:34 -0400

    alter quart

--- index.js
+++ index.js
@@ -168,8 +168,8 @@ export default (e) => {
               egirlSign = gltf.scene;
               gltf.scene.position.copy(params.logoPos);
               // gltf.scene.rotation.set(1.57, 1.57, 0);
-              gltf.scene.quarternion.setFromAxisAngle(new THREE.Vector3(0,1,0), PI/4);
-              gltf.scene.quarternion.setFromAxisAngle(new THREE.Vector3(1,0,0), PI/4);
+              gltf.scene.quarternion.set(new THREE.Vector3(1,1,0), PI/4);
+              // gltf.scene.quarternion.setFromAxisAngle(new THREE.Vector3(1,0,0), PI/4);
               eGirlText = child;
               console.log(eGirlText);
               // gltf.scene.quaternion.copy(params.logoQuat);
